adb8a751d97acccab145976901caa676adcd5f8e
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-09 11:00:51 +0100

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useEffect, useRef, useState } from 'react';
+import './App.scss';
+
+
+function App() {
+
+	return (
+		<div className="">
+			<h1>Hello world!</h1>
+
+
+
+
+			<p className="text-primary text-center p-4 font-medium">created by <span className='font-medium text-cyan-200'>houssam-developer</span> - devChallenges.io</p>
+
+		</div>
+	)
+}
+
+export default App;
