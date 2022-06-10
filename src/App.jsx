c3690f79c61582949a849e3823bb7f3046adf93d
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-10 07:30:24 +0100

    refactor: add radio buttons for location

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { apiService } from './services/ApiService';
 import { MdWorkOutline } from "react-icons/md";
+import { BiWorld } from "react-icons/bi";
 
 
 function App() {
@@ -20,19 +21,59 @@ function App() {
 				<form>
 
 					{/* container search */}
-					<div className='px-3 py-10 container-search-input rounded-md'>
-						<div className='container-search-input__subcontainer'>
+					<div className='px-3 py-10 container-input-search rounded-md'>
+						<div className='container-input-search__subcontainer'>
 							<MdWorkOutline className='min-w-[14px]' size={24} />
 							<input className='font-[Roboto] font-normal text-xs text-[#334680]' type="text" name="search" placeholder='Title, companies, expert' />
 							<button className='bg-[#1E86FF] text-xs text-white py-3 px-4 rounded ml-auto' type='submit'>Search</button>
 						</div>
 					</div>
 
-					{/* fullTime */}
-					<div className='p-4 text-sm font-medium flex items-center gap-2'>
-						<input type="checkbox" name="fulltime" id='fulltime' />
-						<label htmlFor="fulltime">Full time</label>
+					{/* CONTAINER { Fulltime - Location } --- { Results } */}
+					<div>
+
+						<div>
+							{/* Fulltime */}
+							<div className='p-4 text-sm font-medium flex items-center gap-2'>
+								<input type="checkbox" name="fulltime" id='fulltime' />
+								<label htmlFor="fulltime">Full time</label>
+							</div>
+
+							{/* Location */}
+							<div>
+								<h2 className='p-2 text-[#B9BDCF] text-sm font-bold'>Location</h2>
+								<div className='container-input-location'>
+									<BiWorld className='min-[20px]' size={20} />
+									<input type="text" placeholder='City, state, zip code or country' />
+								</div>
+
+								<ul className='p-3 font-medium text-sm'>
+									<li className='p-1 flex items-center gap-2'>
+										<input type="radio" name="city" id="cityOne" />
+										<label htmlFor="cityOne">London</label>
+									</li>
+									<li className='p-1 flex items-center gap-2'>
+										<input type="radio" name="city" id="cityTwo" />
+										<label htmlFor="cityTwo">Amsterdam</label>
+									</li>
+									<li className='p-1 flex items-center gap-2'>
+										<input type="radio" name="city" id="cityThree" />
+										<label htmlFor="cityThree">New York</label>
+									</li>
+									<li className='p-1 flex items-center gap-2'>
+										<input type="radio" name="city" id="cityFour" />
+										<label htmlFor="cityFour">Berlin</label>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div>
+							{/* Results */}
+
+						</div>
 					</div>
+
 
 				</form>
 			</main>
