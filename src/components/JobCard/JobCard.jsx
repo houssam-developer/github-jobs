dfeb9bf79feb982d981b55342ccba54bd942753d
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-10 09:04:37 +0100

    refactor: update JobCard template

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -2,27 +2,37 @@
 import { MdAccessTime } from "react-icons/md";
 import { BiWorld } from "react-icons/bi";
 
-function JobCard({ }) {
+function JobCard(
+	{
+		companyPicture = 'company-picture.png',
+		companyName = 'CompanyName',
+		jobTitle = 'JobTitle',
+		jobTimeType = 'JobTimeType',
+		jobLocation = 'JobCity',
+		jobPostedDays = 'JobDays Ago'
+
+	}) {
 	return (
 		<article className="p-2 bg-white shadow rounded-md font-['Roboto'] text-[#334680] flex gap-1 sm:gap-2 md:gap-4">
 			<div>
-				<img className="max-w-[90px] max-h-auto object-contain rounded-md border-2 border-orange-400" src="./images/companies/gfi.png" alt="picture of an company" />
+				<img className="max-w-[90px] max-h-auto object-contain rounded-md border-[1px] border-orange-400"
+					src={`./images/companies/${companyPicture}`} alt="picture of an company" />
 			</div>
 
 			<div className="flex flex-col gap-4">
 				<div className="flex flex-col gap-2">
-					<h3 className=" text-xs font-bold">Kasito</h3>
-					<p className="text-xs sm:text-sm md:text-base">Front-End Software Engineer</p>
+					<h3 className=" text-xs font-bold">{companyName}</h3>
+					<p className="text-xs sm:text-sm md:text-base">{jobTitle}</p>
 				</div>
-				<span className="px-[.5em] py-1 text-xs sm:text-sm font-bold border-[1px] border-[#334680] rounded self-start">Full time</span>
+				<span className="px-[.5em] py-1 text-xs sm:text-sm font-bold border-[1px] border-[#334680] rounded self-start">{jobTimeType}</span>
 				<div className="py-2 text-xs text-[#B7BCCE] font-medium flex items-center gap-2 sm:gap-4">
 					<div className="flex items-center gap-1">
 						<BiWorld className='min-[14px]' size={14} />
-						<span>New York</span>
+						<span>{jobLocation}</span>
 					</div>
 					<div className="flex items-center gap-1">
 						<MdAccessTime className='min-[14px]' size={14} />
-						<span>5 days ago</span>
+						<span>{jobPostedDays}</span>
 					</div>
 				</div>
 			</div>
